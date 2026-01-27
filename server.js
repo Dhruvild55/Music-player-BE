a65c027dd431e0a0ed6da071b745af1f90d4e0b1
dhruvild55 <dhruvild@ysoftsolution.com>
2026-01-27 12:35:07 +0530

    change in server.js  CORS part

--- server.js
+++ server.js
@@ -13,23 +13,11 @@ dotenv.config();
 
 const app = express();
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://localhost:4173", // Vite preview port
     process.env.FRONTEND_URL
 ].filter(Boolean);
 
 app.use(cors({
-    origin: (origin, callback) => {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin: true,
     credentials: true
 }));
 app.use(express.json());
